b15d6e639311d01c59f425889171e518552f9b68
ChrisHorrigan <cdhorrigan@gmail.com>
2015-12-18 11:08:16 -0800

    added the stuff

---
+++ VRtut/VR Tutorial/Assets/Cardboard/Scripts/MoveForwardOnTap.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveForwardOnTap : MonoBehaviour {
+
+	public GameObject head;
+	float speed = 2f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+ 		if (Input.GetKey (KeyCode.Space) || Input.GetTouch (0).phase == TouchPhase.Stationary) {
+
+			transform.position += speed * head.transform.forward * Time.deltaTime;
+		}
+	}
+}
